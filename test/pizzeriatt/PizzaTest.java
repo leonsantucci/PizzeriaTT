afc30056138fa6a913e2d476e906adb93790e4ee
marcos122696 <39200486+marcos122696@users.noreply.github.com>
2018-05-10 16:36:43 -0300

    Se cambio el error de pizzaTest

--- test/pizzeriatt/PizzaTest.java
+++ test/pizzeriatt/PizzaTest.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pizzeria;
+package pizzeriatt;
 
 import java.util.function.UnaryOperator;
 import org.junit.Test;
@@ -22,26 +17,26 @@ public class PizzaTest {
 
     @Test(expected = Error.class)
     public void deberiaDarErrorAlIngresarUnaPizzaDe9Porciones() {
-        Pizza unaPizza = new Pizza(new Variedad("muzarella", 70), 9, Tipo.MOLDE);
+        Pizza unaPizza = new Pizza(9, Tipo.MOLDE, new Variedad("Mozarella", 70));
     }
 
     @Test
     public void deberiaValer200LaMuzarellaDe8PorcionesAlaParilla() {
-        Pizza unaPizza = new Pizza(new Variedad("muzarella", 70), 8, Tipo.PARRILLA);
+        Pizza unaPizza = new Pizza(8, Tipo.PARRILLA, new Variedad("mozarella", 70));
         double valorPizza = unaPizza.getPrecio();
         assertEquals(200, valorPizza, 0.001);
     }
 
     @Test
     public void deberiaValer245LaPizzaDeMuzarellaDe12PorcionesAElMolde() {
-        Pizza unaPizza = new Pizza(new Variedad("muzarella", 70), 12, Tipo.MOLDE);
+        Pizza unaPizza = new Pizza(12, Tipo.MOLDE, new Variedad("4 Quesos", 70));
         double valorPizza = unaPizza.getPrecio();
         assertEquals(245, valorPizza, 0.001);
     }
 
     @Test
     public void deberiaValer250LaPizzaDe4QuesosDe12PorcionesAElMolde() {
-        Pizza unaPizza = new Pizza(new Variedad("4 QUESOS", 75), 12, Tipo.MOLDE);
+        Pizza unaPizza = new Pizza(12, Tipo.MOLDE, new Variedad("4 Quesos", 75));
         double valorPizza = unaPizza.getPrecio();
         assertEquals(250, valorPizza, 0.001);
     }
